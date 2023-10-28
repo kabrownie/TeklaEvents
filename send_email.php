9e0fc686cb7b2139198c46f11ece7c9d23b0be8e
kabrownie <makonokevin@gmail.com>
2023-10-28 17:17:22 +0300

    php

--- send_email.php
+++ send_email.php
@@ -2,8 +2,8 @@
 if (isset($_POST['email'])) {
 
     // REPLACE THIS 2 LINES AS YOU DESIRE
-    $email_to = "you@awesomecompany.com";
-    $email_subject = "You've got a new submission";
+    $email_to = "kabrowniebeats@gmail.com";
+    $email_subject = "Tekla Events";
 
     function problem($error)
     {
